5b3a3070ab49d0176b9af2eca48a6de970d4cc97
Rondinely Ribeiro Miranda <roondinely@gmail.com>
2025-05-11 12:58:20 +0000

    teste 11.5

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,9 @@
 //Teste larissa
 
 int main() {
+
+    printf("teste de salvamento\n");
+    
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
